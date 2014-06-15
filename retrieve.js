7042641a408817159b51965d27785202dc753c74
Jesper Kristensen <jesper.kristensen@capgemini.com>
2014-06-15 16:46:40 +0200

    rename main file and move login to separate file

--- retrieve.js
+++ retrieve.js
@@ -1,8 +1,8 @@
-var jsforce = require("jsforce");
 var fs = require("fs");
 var Promise = require("jsforce/lib/promise");
 var q = require("q");
 var JSZip = require("jszip");
+var common = require("./common");
 
 function asArray(x) {
   if (!x) return [];
@@ -30,24 +30,9 @@ function writeFile(path, data) {
 }
 
 var conn;
-Promise
-  .all([
-    q.nfcall(fs.readFile, "forcecmd.json", "utf-8"),
-    q.nfcall(fs.readFile, (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + "/forcepw.json", "utf-8")
-  ])
-  .then(function(files) {
-    var file = files[0];
-    var pwfile = files[1];
-    var config = JSON.parse(file);
-    var password = JSON.parse(pwfile).passwords[config.loginUrl + "$" + config.username];
-    if (!config.loginUrl) throw "Missing loginUrl";
-    if (!config.username) throw "Missing username";
-    if (!password) throw "Missing password";
-    conn = new jsforce.Connection({loginUrl: config.loginUrl, version: "28.0"});
-    console.log("Login");
-    return conn.login(config.username, password);
-  })
-  .then(function() {
+common.login()
+  .then(function(c) {
+    conn = c;
     console.log("Describe");
     return conn.metadata.describe("28.0");
   })
